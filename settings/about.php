fcc833fd9f8c857ba89436ed56866d075a350ab3
Kasper Isager <kasperisager@gmail.com>
2013-03-23 13:31:04 +0100

    Add application URL and change author URL

--- settings/about.php
+++ settings/about.php
@@ -3,8 +3,9 @@
 $ApplicationInfo['API'] = array(
     'Description'   => 'RESTful API for Vanilla that responds in JSON and XML.',
     'Version'       => '0.1.0',
+    'Url'           => 'https://github.com/kasperisager/VanillaAPI',
     'Author'        => 'Kasper Kronborg Isager',
     'AuthorEmail'   => 'kasperisager@gmail.com',
-    'AuthorUrl'     => 'https://github.com/kasperisager/VanillaAPI',
+    'AuthorUrl'     => 'https://github.com/kasperisager',
     'License'       => 'MIT'
 );
