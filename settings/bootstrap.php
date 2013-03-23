5dac87b9bbdbd719961619a1d2293fb59d70621c
Kasper Isager <kasperisager@gmail.com>
2013-03-23 19:51:16 +0100

    Use Composer for managing 3rd party libraries

--- settings/bootstrap.php
+++ settings/bootstrap.php
@@ -1,10 +1,4 @@
 <?php if (!defined('APPLICATION')) exit();
 
-// Bootstrap Swagger PHP
-require PATH_APPLICATIONS . '/api/vendor/Swagger/Swagger.php';
-
-// Bootstrap Doctrine Common
-use Doctrine\Common\ClassLoader;
-require PATH_APPLICATIONS . '/api/vendor/Doctrine/Common/ClassLoader.php';
-$commonLoader = new ClassLoader('Doctrine\Common', PATH_APPLICATIONS . '/api/vendor');
-$commonLoader->register();
+// Bootstrap vendor libraries
+require PATH_APPLICATIONS . '/api/vendors/autoload.php';
