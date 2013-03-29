8f6ae4d301aadc42932d34f57240a733da34eee0
Kasper Isager <kasperisager@gmail.com>
2013-03-30 00:01:00 +0100

    The API is not entirely RESTful yet so change description to REST-like

--- controllers/class.apicontroller.php
+++ controllers/class.apicontroller.php
@@ -3,7 +3,7 @@
 /**
  * Vanilla API Main controller
  *
- * The Vanilla API lets you interface with Vanilla in a fully RESTful way
+ * The Vanilla API lets you interface with Vanilla in a REST-like way
  * using the standard HTTP verbs GET, POST, PUT and DELETE.
  *
  * @package    API
