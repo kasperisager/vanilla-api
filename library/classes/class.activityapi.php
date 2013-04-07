e84221b6bc1090b62c67e6b8b2c4cda219bb5199
Kasper Isager <kasperisager@gmail.com>
2013-04-07 18:57:43 +0200

    Skeleton APIs for the unfinished classes

---
+++ library/classes/class.activityapi.php
@@ -0,0 +1,63 @@
+<?php if (!defined('APPLICATION')) exit();
+
+//use Swagger\Annotations as SWG;
+
+/**
+ * Activity API
+ *
+ * @package    API
+ * @since      0.1.0
+ * @author     Kasper Kronborg Isager <kasperisager@gmail.com>
+ * @copyright  Copyright 2013 © Kasper Kronborg Isager
+ * @license    http://opensource.org/licenses/MIT MIT
+ *
+ * @SWG\resource(
+ *   resourcePath="/activity"
+ * )
+ */
+class ActivityAPI extends APIMapper
+{
+   /**
+    * GET
+    *
+    * @since   0.1.0
+    * @access  public
+    */
+   public function Get($Parameters)
+   {
+      throw new Exception("Method Not Implemented", 501);
+   }
+
+   /**
+    * POST
+    *
+    * @since   0.1.0
+    * @access  public
+    */
+   public function Post($Parameters)
+   {
+      throw new Exception("Method Not Implemented", 501);
+   }
+
+   /**
+    * PUT
+    *
+    * @since   0.1.0
+    * @access  public
+    */
+   public function Put($Parameters)
+   {
+      throw new Exception("Method Not Implemented", 501);
+   }
+
+   /**
+    * DELETE
+    *
+    * @since   0.1.0
+    * @access  public
+    */
+   public function Delete($Parameters)
+   {
+      throw new Exception("Method Not Implemented", 501);
+   }
+}
