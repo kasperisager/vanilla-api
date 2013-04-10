102707cc241ae79c01d3fe67f71e66d8f363278c
Kasper Isager <kasperisager@gmail.com>
2013-04-11 00:48:15 +0200

    Deprecate the API autoloader in favor of using Garden's autoloader

--- library/class.api_autoload.php
+++ library/class.api_autoload.php
@@ -12,6 +12,7 @@
  * @author     Kasper Kronborg Isager <kasperisager@gmail.com>
  * @copyright  Copyright 2013 © Kasper Kronborg Isager
  * @license    http://opensource.org/licenses/MIT MIT
+ * @deprecated
  */
 class API_Autoload
 {
