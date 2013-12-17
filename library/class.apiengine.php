db1db13b77d9e2d9ffce5db2e5531e6c4e22ed37
Kasper Kronborg Isager <kasperisager@gmail.com>
2013-12-16 21:20:53 -0500

    Don't allow subclassing for now

--- library/class.apiengine.php
+++ library/class.apiengine.php
@@ -6,13 +6,18 @@
  * This class handles authentication and delegation of API requests and their
  * corresponding methods.
  *
+ * The API engine itself is currently closed off for subclassing. This will
+ * hopefully change in the future once the engine is stable and ready for
+ * general consumption and extension.
+ *
  * @package   API
  * @since     0.1.0
  * @author    Kasper Kronborg Isager <kasperisager@gmail.com>
  * @copyright Copyright 2013 © Kasper Kronborg Isager
  * @license   http://opensource.org/licenses/MIT MIT
+ * @final
  */
-class APIEngine
+final class APIEngine
 {
     /**
      * Token-based, per-request authentication
