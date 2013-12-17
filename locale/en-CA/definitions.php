263956274d7adf388d4ec6f9b590cc58f7a75bd0
Kasper Kronborg Isager <kasperisager@gmail.com>
2013-12-16 23:10:57 -0500

    Use inline comments for the locale definition placeholders

--- locale/en-CA/definitions.php
+++ locale/en-CA/definitions.php
@@ -1,13 +1,12 @@
 <?php if (!defined('APPLICATION')) exit;
-/*
-$Definition["Endpoint"] = "";
-$Definition["Application Interface"] = "";
-$Definition["Application Secret"] = "";
-$Definition["Read the API documentation"] = "";
-$Definition["Refresh the page to see the new Application Secret."] = "";
-$Definition["You can access your forum's Application Interface (API) through this endpoint URL"] = "";
-$Definition["This is the Application Secret used for signature based authentication. <b>Keep it secret!</b>"] = "";
-$Definition["Clicking \"Re-generate\" will generate a new UUID v4 key. Please refer to %s for more information"] = "";
-$Definition["this article"] = "";
-$Definition["Re-generate"] = "";
-*/
+
+// $Definition["Endpoint"] = "";
+// $Definition["Application Interface"] = "";
+// $Definition["Application Secret"] = "";
+// $Definition["Read the API documentation"] = "";
+// $Definition["Refresh the page to see the new Application Secret."] = "";
+// $Definition["You can access your forum's Application Interface (API) through this endpoint URL"] = "";
+// $Definition["This is the Application Secret used for signature based authentication. <b>Keep it secret!</b>"] = "";
+// $Definition["Clicking \"Re-generate\" will generate a new UUID v4 key. Please refer to %s for more information"] = "";
+// $Definition["this article"] = "";
+// $Definition["Re-generate"] = "";
