184a62deb8aed76bdf84927a789b77ef09fe9618
Kasper Isager <kasperisager@gmail.com>
2015-01-08 14:28:27 +0100

    Bump version to 0.3.0

--- settings/about.php
+++ settings/about.php
@@ -3,7 +3,7 @@
 $ApplicationInfo['api'] = [
     'Name'        => 'Vanilla API',
     'Description' => 'Extensible RESTful API shim for Vanilla that operates in JSON or XML. Supports JSONP and CORS',
-    'Version'     => '0.2.0',
+    'Version'     => '0.3.0',
     'Url'         => 'https://github.com/kasperisager/vanilla-api',
     'Author'      => 'Kasper Kronborg Isager',
     'AuthorEmail' => 'kasperisager@gmail.com',
