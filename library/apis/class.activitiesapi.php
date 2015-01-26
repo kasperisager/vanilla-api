d803f0127ba98126d3c1b36ed68a50b982c4ad86
Kasper Isager <kasperisager@gmail.com>
2015-01-26 16:37:54 +0100

    Update copyright notices.

--- library/apis/class.activitiesapi.php
+++ library/apis/class.activitiesapi.php
@@ -6,7 +6,7 @@
  * @package   API
  * @since     0.1.0
  * @author    Kasper Kronborg Isager <kasperisager@gmail.com>
- * @copyright Copyright 2013 © Kasper Kronborg Isager
+ * @copyright Copyright (c) 2013-2015 Kasper Kronborg Isager
  * @license   http://opensource.org/licenses/MIT MIT
  */
 class ActivitiesAPI extends APIMapper
